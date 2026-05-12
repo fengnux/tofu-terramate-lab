ab03ba2e9503935f862a46754e7d2021af8a21cd
feng liu <fengnux@gmail.com>
2026-05-12 22:31:40 +0800

    feat(dev/apis): 啟用 compute API

---
+++ stacks/dev/apis/main.tf
@@ -0,0 +1,12 @@
+locals {
+  enabled_apis = [
+    "compute.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "this" {
+  for_each = toset(local.enabled_apis)
+
+  service            = each.value
+  disable_on_destroy = false
+}
---
+++ stacks/dev/apis/stack.tm.hcl
@@ -0,0 +1,6 @@
+stack {
+  id          = "apis"
+  name        = "apis"
+  description = "啟用 dev project 需要的 GCP API"
+  tags        = ["dev", "apis"]
+}
